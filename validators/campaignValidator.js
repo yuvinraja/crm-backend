7389e3b84c2ca859ff4cf975a45fb3ea43553d8f
Yuvin Raja <yuvinrajav@gmail.com>
2025-09-16 23:25:28 +0530

    remove one line again

--- validators/campaignValidator.js
+++ validators/campaignValidator.js
@@ -10,7 +10,6 @@ const campaignCreateSchema = z.object({
   name: z.string().min(1, 'Campaign name is required'),
   segmentId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid segmentId'),
   message: z.string().min(1, 'Message is required'),
-  createdBy: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid userId'),
   stats: statsSchema.optional(),
 });
 
